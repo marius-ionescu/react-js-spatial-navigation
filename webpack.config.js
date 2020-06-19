9c12a4ae01f2eab2ee1c64d799f81623fde2b094
Marius Ionescu <Marius.Ionescu@endava.com>
2020-06-19 15:47:53 +0300

    Initial changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,13 @@
-var path = require('path');
-
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2'
-  },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|bower_components|build)/,
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env', 'stage-0']
-          }
-        }
-      }
-    ]
+          loader: "babel-loader",
+        },
+      },
+    ],
   },
-  externals: {
-    'react': 'commonjs react'
-  }
 };

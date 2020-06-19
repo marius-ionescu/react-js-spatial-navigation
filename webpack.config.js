5136382aa927b552de27296d3768b907a8dbdc2c
Marius Ionescu <Marius.Ionescu@endava.com>
2020-06-19 17:54:42 +0300

    small change

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
+var path = require("path");
+
 module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
+  },
   module: {
     rules: [
       {
@@ -9,5 +17,8 @@ module.exports = {
         },
       },
     ],
+  },
+  externals: {
+    react: "commonjs react",
   },
 };
